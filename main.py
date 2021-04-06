ddb35e818a8fd76941892da6ebfae68565eaaed2
sankyyyyy <sanketsalvi2001@gmail.com>
2021-04-06 22:43:48 +0530

    modified

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pyttsx3
 import datetime
+#from datetime import*
 import speech_recognition as sr 
+import wikipedia
+import webbrowser
+import os
+from random import*
 
 engine = pyttsx3.init('sapi5')
 voice = engine.getProperty('voices')
@@ -12,7 +17,7 @@ def speak(audio):
     engine.runAndWait()
 
 def wishme():
-    hour = int(datetime.now().hour)
+    hour = int(datetime.datetime.now().hour)
     if hour >=0 and hour <=12:
         speak("Good Morning")
     elif hour >= 12 and hour <=18:
@@ -32,16 +37,57 @@ def takecommand():
 
     try:
         print("Recognizing...")
-        quert = r.recognize_google(audio)
-        print(f"User said:{quert}\n")
+        query = r.recognize_google(audio)
+        print(f"User said:{query}\n")
     except Exception as e:
         print(e)
         print("Say that again please...")
         return "None"
-    return quert
+    return query
 
 
 
 if __name__ == "__main__":
-    speak("run succesfully")
-    takecommand()
+    wishme()
+    #speak("run succesfully")
+    query= takecommand().lower()
+    #logic for executing task
+    if 'wikipedia' in query:
+        try:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query,sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+        except Exception as e:
+            #print(e)
+            print("Result not found!")
+    elif 'open youtube' in query:
+        webbrowser.open("youtube.com")
+    
+    elif 'open google' in query:
+        webbrowser.open("google.com")
+
+    elif 'open github' in query:
+        webbrowser.open("github.com")
+
+    elif 'play music' in query:
+        music_dir = "D:\\2\\songs"
+        #set path of your desktop for songs
+        random_index = randint(1,100)
+        songs = os.listdir(music_dir)
+        print(songs)
+        os.startfile(os.path.join(music_dir,songs[random_index]))
+    
+    elif 'time' in query:
+        strTime = datetime.datetime.now()
+        strTime.strftime("%H-%M")
+        print(strTime)
+        speak(f"Sir,the time is{strTime}")
+
+    elif 'open code' in query:
+        #set your path here in codePath
+        codePath = "C:\\Users\\sanke\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+        os.startfile(codePath)
+
